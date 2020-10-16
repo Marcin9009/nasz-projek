1c2e1abd580cb7aeaf276eca0ccf9fa871e98914
Marcin9009 <60286301+Marcin9009@users.noreply.github.com>
2020-10-16 19:53:55 +0200

    Add files via upload

---
+++ operator_logiczny1.py
@@ -0,0 +1,12 @@
+# print(100 == 100.0)
+# print(100 == 1e2)
+# print(100.0 == 1e2)
+#print(0.1 + 0.2 == 0.3)
+# print(round(0.1 + 0.2, 1) == 0.3)
+result_1 ="Alice" == "Alice"
+result_2 ="Alice" == "Alice"
+result_3 = 2 < 10
+
+print(result_1)
+print(result_2)
+print(result_3)
